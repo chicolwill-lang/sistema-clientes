b6e08c3b5c06ef9898c28ca6ef0b2ba50a609313
chicolwill-lang <chicolwill@gmail.com>
2026-02-08 22:46:40 -0600

    Add files via upload

--- app.py
+++ app.py
@@ -52,5 +52,8 @@ def agregar_cliente():
 
     return render_template("agregar.html")
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
